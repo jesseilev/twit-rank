c48041e505bb1c6bef67126e1ca2d49a9d41dc7f
jesseilev <jesseilev@gmail.com>
2014-07-03 17:08:47 -0500

    Small change
    rename ‘id’ iVar to ‘hid’; change value of damping_factor

--- Twit.rb
+++ Twit.rb
@@ -1,36 +1,36 @@
 
 class User
 
-    def initialize( name, id )
+    def initialize( name, uid )
         puts 'Creating a new user named ' + name
         @name = name
-        @id = id
+        @uid = uid
         @followers = []
         @leaders = []
         @twit_rank = 0.0
     end
     
     def follow other_user
-        if (self.id == other_user.id)
+        if (self.uid == other_user.uid)
             #Can't follow yourself.
             return
         end
-        if (self.leaders.include? other_user.id)
+        if (self.leaders.include? other_user.uid)
             #Self is already following other_user.
             return
         end
         
         puts self.name + ' has begun following ' + other_user.name + ' .'
-        self.leaders << other_user.id
-        other_user.followers << self.id
+        self.leaders << other_user.uid
+        other_user.followers << self.uid
     end
     
     def name
         @name
     end
     
-    def id
-        @id
+    def uid
+        @uid
     end
     
     def followers
@@ -57,40 +57,40 @@ end
 class TwitterGraph
 
     def initialize( leaders_hash )
-        # hash :id => User
+        # hash :uid => User
         @users = {}
         
-        # hash :id => twit_rank
+        # hash :uid => twit_rank
         @scores = {}
         
-        # hash :name => id
-        @ids_hash = {}
+        # hash :name => uid
+        @uids_hash = {}
         
         #Iterate twice over leadersHash
         #First time, create all the users
-        next_id = 0
+        next_uid = 0
         leaders_hash.keys.each do |name, leaders|
-            new_user = User.new(name, next_id)
-            @users[next_id] = new_user
-            @ids_hash[new_user.name] = new_user.id
+            new_user = User.new(name, next_uid)
+            @users[next_uid] = new_user
+            @uids_hash[new_user.name] = new_user.uid
 
             new_user.twit_rank = 1.0 / Float(leaders_hash.keys.count)
-            @scores[new_user.id] = new_user.twit_rank
+            @scores[new_user.uid] = new_user.twit_rank
             
-            next_id += 1
+            next_uid += 1
         end
         
         #Second time, make them follow each other
         leaders_hash.each do |follower_name, leaders|
-            follower = @users[@ids_hash[follower_name]]
+            follower = @users[@uids_hash[follower_name]]
             leaders.each do |leader_name|
-                leader = @users[@ids_hash[leader_name]]
+                leader = @users[@uids_hash[leader_name]]
                 follower.follow leader
             end
             
             #If a user is not following anybody, then we pretend he is following EVERYBODY. "When calculating PageRank, pages with no outbound links are assumed to link out to all other pages in the collection. Their PageRank scores are therefore divided evenly among all other pages."
             if (leaders.count == 0)
-                users.each do |id, user|
+                users.each do |uid, user|
                     follower.follow user
                 end
             end
@@ -103,29 +103,29 @@ class TwitterGraph
         num_iterations.times do
             puts 'Computing scores. Iteration ' + current_iteration.to_s + '......................................'
             #Compute everyone's score and store it in @scores
-            users.each do |id, user|
+            users.each do |uid, user|
                 score = 0.0
                 
                 #Aggregate score from followers' scores
-                user.followers.each do |follower_id|
-                    follower = @users[follower_id]
+                user.followers.each do |follower_uid|
+                    follower = @users[follower_uid]
                     #If my follower has n leaders, I get 1/nth of his score
                     score += follower.twit_rank / Float(follower.leaders.count)
                     
                 end
                 
                 #Apply damping factor to the resulting score
-                damping_factor = 0.99
+                damping_factor = 0.85
                 dampened_score = damping_factor * score
                 random_jump = (1.0 - damping_factor) / Float(users.count)
                 final_score = dampened_score + random_jump
                 
-                @scores[id] = final_score
+                @scores[uid] = final_score
             end
             
             #Loop through @scores and set everyone's .twit_rank
-            @scores.each do |id, score|
-                user = @users[id]
+            @scores.each do |uid, score|
+                user = @users[uid]
                 user.twit_rank = score
             end
             
@@ -154,7 +154,7 @@ leaders_hash = {
                 }
 
 graph = TwitterGraph.new(leaders_hash)
-graph.compute_scores 85
+graph.compute_scores 35
 
 
 
